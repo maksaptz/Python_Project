93995a04f39755c41e2a935011c61491e10b2197
maksa <maksa_ptz@mail.ru>
2024-12-14 14:26:27 +0300

    Initial commit

---
+++ Module_1/module_1_6.py
@@ -0,0 +1,5 @@
+homework_count = 12
+hours_count = 1.5
+name_of_course = "Python"
+time_to_one_task = hours_count / homework_count
+print("Курс:", name_of_course, ", всего задач:", homework_count, ", затрачено часов:", hours_count, ", среднее время выполнения ", time_to_one_task, " часа.", sep = '')
