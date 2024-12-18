9fc74e8bbf683d1ee8ac11f2fead773e979f8ce5
maksa <maksa_ptz@mail.ru>
2024-12-18 20:34:34 +0300

    Initial commit

--- Module_1/module_1_5.py
+++ Module_1/module_1_5.py
@@ -4,6 +4,7 @@ temp_list = list(immutable_var)  # Преобразуем кортеж в спи
 temp_list[0] = 2 # Изменяем элемент в списоке
 immutable_var = tuple(temp_list)  # Возвращаем данные обратно в кортеж
 print(immutable_var)
-mutable_list = [1, 2, 3]
-mutable_list.append(123)
+mutable_list = [1, 2]
+mutable_list[0] = 4
+mutable_list[1] = 3
 print(mutable_list)
