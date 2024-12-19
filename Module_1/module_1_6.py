c51cac722768fb59693700a9ecd9acc1d60a0d18
maksa <maksa_ptz@mail.ru>
2024-12-19 20:42:29 +0300

    Initial commit

---
+++ Module_1/module_1_6.py
@@ -0,0 +1,16 @@
+# Dictionary
+my_dict = {1: 'january', 2: 'february'}
+print(my_dict)
+print(my_dict[1])
+print(my_dict.get(11))
+my_dict.update({4: 'april',
+                5: 'may'})
+print(my_dict.pop(2))
+print(my_dict)
+# Set
+my_set = {1, 2, 2, 'Good', False ,'Good', False}
+print(my_set)
+my_set.add(4)
+my_set.add('Color')
+my_set.remove(1)
+print(my_set)
