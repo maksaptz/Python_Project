63a8f2bff3b562d377a8187b17016da915840ef2
maksa <maksa_ptz@mail.ru>
2024-12-26 15:22:04 +0300

    Initial commit

---
+++ Module_2/module_2_5.py
@@ -0,0 +1,20 @@
+# Задаем функцию
+def get_matrix(n, m, value):
+    # Создаем пустой список
+    matrix = []
+    # Внешний цикл
+    for i in range(n):
+        # Добавляем пустую матрицу
+        matrix.append([])
+        # Внутренний цикл
+        for j in range(m):
+        # Наполняем созданную матрицу значениями
+            matrix[i].append(value)
+    # Возвращаем результат
+    return matrix
+
+# Выводим ответы на консоль
+print(get_matrix(2,2,10 ))
+print(get_matrix(3,5,42 ))
+print(get_matrix(4,2,13 ))
+print(get_matrix(0,0,0 ))
